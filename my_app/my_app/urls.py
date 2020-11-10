78ba0a4e28590d01eb7fd65faf07aabd8355570a
jraikhola <jraikhola@gmail.com>
2020-11-10 21:42:12 +0545

    django ecom website and blog site update

--- my_app/my_app/urls.py
+++ my_app/my_app/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls'))
 
